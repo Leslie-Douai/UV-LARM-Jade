3d49a21f581d81f5190efa33f4e6df922a278d0d
RINEAU Leslie <leslie.rineau@etu.imt-lille-douai.fr>
2022-01-17 17:10:31 +0100

    oui

--- grp-jade-challenge2/scripts/challenge2_1.py
+++ grp-jade-challenge2/scripts/challenge2_1.py
@@ -21,27 +21,26 @@ class BottleDetection():
 		self.cascade = cv.CascadeClassifier("/mnt/Secondaire/catkin-ws/src/UV-LARM-Jade/vision/modele/cascade.xml")
 		self.tfListener = tf.TransformListener()
 		self.markerPublisher = rospy.Publisher('/bottle',Marker, queue_size=10)
-		self.markers_list = list()
 
 	def detectAndDisplay(self, frame):
 		frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-		frame_gray = cv.equalizeHist(frame_gray)
-		color_info = (255, 255, 255)
+		frame_gray = cv.equalizeHist(frame_gray)	
+				
+		# cv.imshow('Capture', frame)
+		# cv.waitKey(300)
 
-		bottles = self.cascade.detectMultiScale(frame_gray, minNeighbors=30, scaleFactor=3)
+		bottles = self.cascade.detectMultiScale(frame_gray)
+		
+		print(type(bottles))
+		print(bottles)
+		
 		for (x, y, w, h) in bottles:
 			center=(x+(w/2),y+(h/2))
 			frame=cv.ellipse(frame,center,(w/2,h/2),(255,0,255),4)
-			faceROI=frame[y:y+h,x:x+w]
-			median = numpy.median(faceROI)
-
-			if median > 170 and y > frame.shape[0] / 3:
-				cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-				estimated_pose = self.Pose(x,y, w, h)
-				self.position_marqueur(estimated_pose)
-				cv.imshow('Capture - Face detection', frame)
-				if cv.waitKey(10) == 27:
-        				break
+			estimated_pose = self.Pose(x,y, w, h)
+			self.position_marqueur(estimated_pose)
+	
+		
 
 	def Pose(self, x, y, w, h):
 		distance = 2000
@@ -66,16 +65,15 @@ class BottleDetection():
 		bridge = CvBridge()
 		try:
 			cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
-			#self.detectAndDisplay(cv_image)
+			self.detectAndDisplay(cv_image)
 		except Exception as err:
-			pass
+			print("Erreur ", err)
 
-	def position_marqueur(self, pose: Pose):
+	"""def position_marqueur(self, pose: Pose):
 		if len(self.markers_list) == 0:
 			pose_marqueur = self.Creation_marqueur(pose)
 			self.markerPublisher.publish(pose_marqueur)
-			self.markers_list.append(pose_marqueur)
-
+			self.markers_list.append(pose_marqueur)"""
 
 	def Creation_marqueur(self, pose: Pose):
 		pose_stamped = PoseStamped ()
@@ -83,7 +81,6 @@ class BottleDetection():
 		pose_stamped.header.frame_id = "map"
 		pose_stamped = self.tfListener.transformPose("map", pose_stamped)
 		marker = Marker()
-		marker.id = len(self.markers_list)
 		marker.type = Marker.CUBE
 		marker.action = Marker.ADD
 		marker.pose = pose_stamped.pose
